10a019abec25c9bc8cd0950afa8a2a8a7d7dbdd6
Pieter Joost van de Sande <pj@born2code.net>
2017-03-25 19:05:50 +0000

    write combined process output as text

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/nlopes/slack"
 	"log"
 	"os"
 	"os/exec"
@@ -8,19 +9,29 @@ import (
 
 func main() {
 	args := os.Args
-	if len(os.Args) == 0 {
+	if len(os.Args) <= 1 {
 		log.Fatal("missing command")
 	}
 
-	name := args[0]
+	name := args[1]
 	arg := make([]string, 0)
 
-	if len(args) > 1 {
-		arg = args[1:]
+	if len(args) > 2 {
+		arg = args[2:]
 	}
 
 	cmd := exec.Command(name, arg...)
 	output, _ := cmd.CombinedOutput()
 
 	os.Stdout.Write(output)
+
+	api := slack.New("xoxp-158986125361-159701941572-159692993154-0ad0370934136efee11baf57f66bff62")
+	api.SetDebug(true)
+	params := slack.PostMessageParameters{}
+
+	_, _, err := api.PostMessage("general", string(output), params)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
